8f5cacd8158d16a9ad794c84cf56b19cac5867d5
Fiqih <fiqih@Fiqihs-MacBook-Air.local>
2026-05-21 16:14:20 +0700

    update: deploy script for fe

--- scripts/deploy-fe.sh
+++ scripts/deploy-fe.sh
@@ -7,7 +7,8 @@ cd $SCRIPT_DIR/../fe
 if [ ! -f .env ]; then
   echo "⚠️ fe/.env tidak ditemukan! Menggunakan default."
 fi
-docker build --platform $PLATFORM -t $DOCKER_USER/routex-fe:$TAG .
+echo "🐳 Packaging into Docker image ($PLATFORM) using Buildx..."
+docker buildx build --pull --no-cache --platform $PLATFORM -t $DOCKER_USER/routex-fe:$TAG --load .
 docker push $DOCKER_USER/routex-fe:$TAG
 
 echo "✅ Frontend pushed."
